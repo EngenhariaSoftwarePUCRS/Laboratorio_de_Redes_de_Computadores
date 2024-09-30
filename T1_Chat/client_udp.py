ffc8d0cb150c6df3bf875039b879b43bca8944b1
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-09-30 00:10:32 -0300

    T1 - Client with ports

--- T1_Chat/client_udp.py
+++ T1_Chat/client_udp.py
@@ -1,18 +1,19 @@
-from socket import *
+from sys import argv
+from socket import socket, AF_INET, SOCK_DGRAM
 
 from config import server_udp
 
 
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+client_socket = socket(AF_INET, SOCK_DGRAM)
 
 
 def main():
     while True:
-        message = input('Input anycase sentence: ')
+        message = input('Input anycase sentence (or enter to update): ')
 
-        clientSocket.sendto(message.encode(), server_udp)
+        client_socket.sendto(message.encode(), server_udp)
         
-        response, _ = clientSocket.recvfrom(2048)
+        response, _ = client_socket.recvfrom(2048)
         response = response.decode()
 
         print(response)
@@ -20,9 +21,19 @@ def main():
 
 if __name__ == '__main__':
     try:
-        main()
+        try:
+            port = int(argv[1])
+            client_socket.bind(('localhost', port))
+        except IndexError:
+            print('Port number not provided')
+        except ValueError:
+            print('Invalid port number')
+        else:
+            main()
     except KeyboardInterrupt:
         print('Client stopped')
+    except Exception as e:
+        print(f'An error occurred: {e}')
     finally:
-        clientSocket.close()
+        client_socket.close()
         exit(0)
