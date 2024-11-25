1906b6d57d47f43f5f3744ff22c3dbe9dfaeb27d
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-11-24 22:38:16 -0300

    TF - Script for getting ip of docker containers

---
+++ TF_SockRAW/scripts/get-container-ips.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Extract the suffix argument
+SUFFIX=$1
+
+# Find the container name matching the suffix
+CONTAINER=$(docker ps --format "{{.Names}}" | grep "tf_sockraw-labredes")
+
+# Check if a matching container is found
+if [ -z "$CONTAINER" ]; then
+    echo "Error: No container found with suffix '${SUFFIX}'."
+    exit 1
+fi
+
+for container in $CONTAINER; do
+    echo "Container: $container"
+    # Extract the IP address of the container and display it
+    docker exec -it "$container" ip a | grep 'inet ' | awk '{print $2}' | cut -d/ -f1
+done
+
+read -p "Press any key to continue..."
