53fff0545637e54204a58120f61af83f7524ad5d
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-11-25 10:51:41 -0300

    TF - Updated Get Container IPs Script (prettier output)

--- TF_ARPSpoofing/scripts/get-container-ips.sh
+++ TF_ARPSpoofing/scripts/get-container-ips.sh
@@ -13,10 +13,16 @@ if [ -z "$CONTAINER" ]; then
     exit 1
 fi
 
+# Print the table header
+echo -e "ID\tAddress\t\tDocker Hostname"
+
 for container in $CONTAINER; do
-    echo "Container: $container"
-    # Extract the IP address of the container and display it
-    docker exec -i "$container" ip a | grep 'inet ' | awk '{print $2}' | cut -d/ -f1
+    # Extract the IP address of the container, excluding loopback address
+    IP_ADDRESS=$(docker exec -i "$container" ip a | grep 'inet ' | awk '{print $2}' | cut -d/ -f1 | grep -v '127\.0\.0\.1')
+    # Extract the ID from the container name
+    ID=$(echo "$container" | awk -F'-' '{print $(NF-1)}' | grep -o '[0-9]*')
+    # Print the table row
+    echo -e "$ID\t$IP_ADDRESS\t$container"
 done
 
 read -p "Press any key to continue..."
